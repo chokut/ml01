14546cd4344ac84822d6903a04f56bc4158ab3e7
chokut <code@io.matrix.jp>
2025-10-12 09:28:48 +0900

    Update test01.py

--- fit/test01.py
+++ fit/test01.py
@@ -12,22 +12,21 @@ import torch
 import matplotlib.pyplot as plt
 
 N = 101
-x = np.linspace(0, 4, N) + np.random.rand(N)
-y = 0.0 + 0.2 * x + np.random.rand(N)
+x = np.linspace(0, 4, N) + np.random.rand(N) - .5
+y = 0.0 + 0.75 * x + np.random.rand(N) - .5
 #print(x)
 #print(y)
 
-#xa = np.c_[np.ones(N), x]
-#xa = x
-#print(xa)
-#print(xa.T @ xa)
+xa = np.c_[np.zeros(N), x]
+a0, a1 = np.linalg.pinv(xa) @ y
+print(a0, a1)
 #t = np.linalg.inv(xa.T @ xa) @ xa.T @ y
 #print(t)
-a = (1. / (x.T @ x) * x.T) @ y
-print(a)
+#a = (1. / (x.T @ x) * x.T) @ y
+#print(a)
 
 x2 = np.array([0, 4.])
-y2 = a * x2
+y2 = a1 * x2
 fig, axes = plt.subplots(1, 1)
 fig.set_size_inches(8, 8)
 
